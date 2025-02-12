98a7e8840dff7c3202c89857238870062a5e1705
pashpashovich <pavel.kosovich2004@gmail.com>
2025-02-12 14:12:28 +0300

    Init commit

---
+++ src/main/java/by/innowise/registrationapp/RegistrationAppApplication.java
@@ -0,0 +1,13 @@
+package by.innowise.registrationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/innowise/registrationapp/RegistrationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package by.innowise.registrationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
